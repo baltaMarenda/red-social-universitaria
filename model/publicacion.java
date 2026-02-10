0a02d6a16dfd188980798572bccbb6bdb9b7c820
Baltazar Marenda <balta.marenda@gmail.com>
2026-02-10 17:34:59 -0300

    Creacion User y Publicacion

---
+++ model/publicacion.java
@@ -0,0 +1,23 @@
+public class publicacion {
+    private String contenido;
+    private int likes;
+    private int shares;
+    private List<String> comentarios;
+    public User autor;
+
+
+    public void likear(){
+
+    }
+    public void comentar(){
+
+    }
+    public void agregarPublicacion(){
+
+    }
+    public void compartir(){
+
+    }
+
+
+}
---
+++ model/user.java
@@ -0,0 +1,22 @@
+public class User{
+    private String nombre;
+    private String apellido
+    private int telefono;
+    private String mail;
+    public List<Publicacion> publicaciones;
+
+    public User(String nombre, String apellido, int telefono, String mail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.mail = mail;
+    }
+
+    public void agregarAmigo(){
+    }
+
+    public void bloquearAmigo(){
+
+    }
+
+}
